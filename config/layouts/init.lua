541d03a6018859026466c3c11ca7a627d8e2b35b
jonesad-etsu <adj.bn99@outlook.com>
2021-08-10 23:10:21 -0400

    Added bling layouts to config.layout

--- config/layouts/init.lua
+++ config/layouts/init.lua
@@ -1,6 +1,10 @@
 local awful = require ('awful')
+local bling = require ('bling')
 awful.layout.layouts = {
 	awful.layout.suit.tile,
 	awful.layout.suit.floating,
 	awful.layout.suit.magnifier,
+    bling.layout.centered,
+    bling.layout.mstab,
+    bling.layout.equalarea,
 }
