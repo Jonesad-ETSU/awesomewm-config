f4553bfcd83d96594acda819991e502131fddc4c
jonesad-etsu <adj.bn99@outlook.com>
2021-09-18 16:17:26 -0400

    Removed some old, junk widgets. Started transitioning to theme configuration through themes.gtk. util.panel_item is the only changed script rn

--- widget/bars/init.lua
+++ widget/bars/init.lua
@@ -1,51 +1,26 @@
 local wibox = require ('wibox')
-local lighten = require ('widget.util.color').lighter
 local pi = require ('widget.util.panel_item')
 local dpi = require ('beautiful.xresources').apply_dpi
-local gears = require ('gears')
 local vol = require ('widget.bars.vol')
 local mic = require ('widget.bars.mic')
 local bri = require ('widget.bars.bri')
 
 local bars = wibox.widget {
-  {
-    vol,
-    shape = gears.shape.rounded_bar,
-    widget = wibox.container.background
+  pi {
+    widget = vol,
+    margins = 0
   },
-  {
-    mic,
-    shape = gears.shape.rounded_bar,
-    widget = wibox.container.background
+  pi {
+    widget = mic,
+    margins = 0,
   },
-  {
-    bri,
-    shape = gears.shape.rounded_bar,
-    widget = wibox.container.background
+  pi {
+    widget = bri,
+    margins = 0,
   },
   spacing = dpi(4),
   layout = wibox.layout.flex.vertical
 }
-
-for _,w in pairs(bars.children) do
-  w:connect_signal(
-    'mouse::enter',
-    function()
-      w.bg = lighten("#585858", 50)
-      w.shape_border_width = 0
-      w.shape_border_color = "#888888"
-    end
-  )
-  w:connect_signal(
-    'mouse::leave',
-    function()
-      w.bg = nil
-      w.shape_border_width = 0
-      w.shape_border_color = nil
-    end
-  )
-end
-
 
 return pi {
   widget = bars,
