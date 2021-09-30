817dff25a1e725cf18f6363620b6cf01e9c97a32
jonesad-etsu <adj.bn99@outlook.com>
2021-09-30 09:27:53 -0400

    Added RAM icon. Change mpd pause to mpc stop in autostart to prevent wait from not detecting first song. Uncommented require('config.notifications') from rc.lua

--- config/notifications/init.lua
+++ config/notifications/init.lua
@@ -4,6 +4,12 @@ local dpi = beautiful.xresources.apply_dpi
 
 naughty.config.defaults.timeout = 5
 naughty.config.defaults.margin = dpi(5)
-naughty.config.defaults.border_width = dpi(2)
-naughty.config.defaults.position = "bottom"
+-- naughty.config.defaults.border_width = dpi(1)
+naughty.config.defaults.border_width = 0
+naughty.config.defaults.position = "bottom_right"
+naughty.config.defaults.title = "System Notification"
+naughty.config.defaults.shape = beautiful.rounded_rect_shape
+naughty.config.spacing = dpi(10)
+
+naughty.config.icon_formats = { 'svg', 'png', 'jpg', 'gif' }
 
